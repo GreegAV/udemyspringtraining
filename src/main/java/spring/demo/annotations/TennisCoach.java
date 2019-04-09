3f4097dc02add497c2eaff175ee0c17aa0278aae
Андрей Григорьев <greegav@gmail.com>
2019-04-09 16:43:55 +0300

    default bean ID

--- src/main/java/spring/demo/annotations/TennisCoach.java
+++ src/main/java/spring/demo/annotations/TennisCoach.java
@@ -2,7 +2,7 @@ package spring.demo.annotations;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
