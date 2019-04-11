4b573314b0723031b7de2177545bbadf892f5589
Андрей Григорьев <greegav@gmail.com>
2019-04-11 12:14:12 +0300

    Annotation @Value

--- src/main/java/spring/demo/annotations/SportConfig.java
+++ src/main/java/spring/demo/annotations/SportConfig.java
@@ -3,9 +3,11 @@ package spring.demo.annotations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("spring.demo.annotations")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
     // define bean for SadFortuneService
--- src/main/java/spring/demo/annotations/SwimCoach.java
+++ src/main/java/spring/demo/annotations/SwimCoach.java
@@ -1,7 +1,15 @@
 package spring.demo.annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
+    @Autowired
     private FortuneService fortuneService;
+    @Value("${foo.email}")
+    private String email;
+    @Value("${foo.team}")
+    private String team;
 
     public SwimCoach(FortuneService theFortuneService) {
         fortuneService = theFortuneService;
@@ -9,7 +17,7 @@ public class SwimCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Swim 1km as a worm up.";
+        return "Swim 1000m as a worm up.";
     }
 
     @Override
